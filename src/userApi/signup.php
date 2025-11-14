ae061a1166422b6d56292bfbba7b1f4eb437ad38
Geider Frias <geiderde20@gmail.com>
2025-11-14 01:23:26 -0300

    update styles homes

--- src/userApi/signup.php
+++ src/userApi/signup.php
@@ -13,7 +13,7 @@ if ($conn->query($userExistsQuery) -> num_rows > 0) {
     exit();
 }
 
-$passwordHash = password_has($password, PASSWORD_BCRYPT);
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
 $addQuery = "INSERT INTO user (name, last_name, email, password, id_role) VALUES ('$name', '$lastName', '$email', '$passwordHash', $idRole)";
 
 if ($conn->query($addQuery)) {
